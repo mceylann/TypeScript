2862cfbec7ae0665183f202c83b36f2a4a8469e3
Mehmet CEYLAN <ceylarino@gmail.com>
2022-11-09 18:08:11 +0300

    public/private & getter/setter

--- 02_pureTS/dist/classes.js
+++ 02_pureTS/dist/classes.js
@@ -8,3 +8,20 @@ class User {
 }
 const mhmt = new User("mehmet", "m@c.com");
 mhmt.city = "Ankara";
+// public/private access control
+class Person {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    getName() {
+        return this.name;
+    }
+    setName(name) {
+        this.name = name;
+    }
+}
+const person = new Person("Pino", "C12/12");
+// person.name -> error
+let pname = person.getName();
+let addr = person.address; // OK - public att.
--- 02_pureTS/src/classes.ts
+++ 02_pureTS/src/classes.ts
@@ -11,3 +11,28 @@ class User {
 
 const mhmt = new User("mehmet","m@c.com");
 mhmt.city = "Ankara";
+
+// public/private access control
+
+class Person {
+    private name: string;
+    public address: string;
+    public constructor(name: string, address: string) {
+      this.name = name;
+      this.address = address;
+    }
+  
+    public getName(): string {
+      return this.name;
+    }
+
+    public setName(name: string){
+        this.name = name;
+    }
+}
+
+const person = new Person("Pino","C12/12");
+
+// person.name -> error
+let pname = person.getName();
+let addr = person.address; // OK - public att.
